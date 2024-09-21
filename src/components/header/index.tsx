4c40c77df0a1f54976b4945ec3b02dd95970339a
Mateus Henrique <mateusdzn10@gmail.com>
2024-09-21 02:04:41 -0400

    feat: :sparkles: adds login and logout feature

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,21 @@
 import Link from 'next/link';
+import { signIn, signOut, useSession } from 'next-auth/react';
+
 import styles from './styles.module.css';
 
 export function Header() {
+  const { data: session, status } = useSession();
+
+  const isLoading = status === 'loading';
+
+  const handleLogin = () => {
+    signIn('google');
+  };
+
+  const handleLogout = () => {
+    signOut();
+  };
+
   return (
     <header className={styles.header}>
       <section className={styles.headerContent}>
@@ -10,12 +24,24 @@ export function Header() {
             <h1 className={styles.logo}>Poeme</h1>
           </Link>
 
-          <Link href="/dashboard" className={styles.dashboardLink}>
-            Meus versos
-          </Link>
+          {session?.user && (
+            <Link href="/dashboard" className={styles.dashboardLink}>
+              Meus versos
+            </Link>
+          )}
         </nav>
 
-        <button className={styles.button}>Entrar</button>
+        {!isLoading && !session && (
+          <button onClick={handleLogin} className={styles.button}>
+            Entrar
+          </button>
+        )}
+
+        {!isLoading && session && (
+          <button onClick={handleLogout} className={styles.button}>
+            Sair
+          </button>
+        )}
       </section>
     </header>
   );
