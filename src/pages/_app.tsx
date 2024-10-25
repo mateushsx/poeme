3df44b07ffa69c55d0095e44b812c0d931cdc4ce
Mateus Henrique <mateusdzn10@gmail.com>
2024-10-25 01:44:04 -0400

    feat: :sparkles: add toast container and import you styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
 import '@/styles/globals.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { SessionProvider } from 'next-auth/react';
+import { ToastContainer } from 'react-toastify';
 
 import { Header } from '@/components/header';
 
 import type { AppProps } from 'next/app';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Header />
+      <ToastContainer />
       <Component {...pageProps} />;
     </SessionProvider>
   );
